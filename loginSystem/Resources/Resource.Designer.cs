41e9f6cf0046362348004545c746cbf3403d5198
Steve Drew <drewypnl@hotmail.com>
2017-04-03 19:33:45 +0100

    Added txtOr text box

--- loginSystem/Resources/Resource.Designer.cs
+++ loginSystem/Resources/Resource.Designer.cs
@@ -63,11 +63,14 @@ namespace loginSystem
 			// aapt resource value: 0x7f050001
 			public const int btnSignIn = 2131034113;
 			
-			// aapt resource value: 0x7f050002
-			public const int btnSignUp = 2131034114;
+			// aapt resource value: 0x7f050003
+			public const int btnSignUp = 2131034115;
 			
 			// aapt resource value: 0x7f050000
 			public const int txtCreateAccount = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int txtOr = 2131034114;
 			
 			static Id()
 			{
